e738808b7f3e29d02040a44f7cbdbc4ef0d634de
Simon <hello@simondaley.co.uk>
2023-08-19 02:25:02 +0100

    fix

--- lib/variables.ts
+++ lib/variables.ts
@@ -1,12 +1,10 @@
-const privateKey = process.env.GITHUB_PRIVATE_KEY.replace(/\\n/g, '\n');
-
 export const env = {
   app_id: process.env.GITHUB_APP_ID,
   client_id: process.env.GITHUB_CLIENT_ID,
   client_secret: process.env.GITHUB_CLIENT_SECRET,
   installation_id: process.env.GITHUB_INSTALLATION_ID,
   token: process.env.GITHUB_TOKEN,
-  private_key: privateKey,
+  private_key: process.env.GITHUB_PRIVATE_KEY,
   encryption_password: process.env.ENCRYPTION_PASSWORD,
   app_host: process.env.NEXT_PUBLIC_GISCUS_APP_HOST as `https://${string}`,
   revalidate_first_page: process.env.NEXT_PUBLIC_REVALIDATE_FIRST_PAGE !== 'false',
