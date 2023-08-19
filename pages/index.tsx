af59629a1cc13b868070d63f90d5c7679e0753d8
Simon <hello@simondaley.co.uk>
2023-08-19 06:13:26 +0100

    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -162,7 +162,19 @@ export default function Home({
         </div>
       </section>
 
-      <div className="color-text-primary w-full max-w-3xl mx-auto p-2 mb-24">
+      <div className="color-text-primary w-full max-w-3xl mx-auto p-2 mb-24 relative">
+        <img
+          className="img-fluid absolute top-20 -right-12 hidden lg:block"
+          src="/img/shape.png"
+          width="60"
+          alt="shape"
+        />
+        <img
+          className="img-fluid absolute -bottom-4 -left-12 hidden lg:block rotate-180"
+          src="/img/shape.png"
+          width="60"
+          alt="shape"
+        />
         <div id="comments" className="giscus w-full my-8" />
         <Script
           src="/client.js"
@@ -179,6 +191,78 @@ export default function Home({
           data-lang="en"
         />
       </div>
+
+      {/*<section className="py-10 bg-white sm:py-16 lg:py-24">
+        <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-semibold text-gray-900">
+              <span className="border-b-4 border-yellow-400">
+                {' '}
+                Trusted by 1200+ world class businesses{' '}
+              </span>
+            </h2>
+          </div>
+
+          <div className="flex mt-16 text-center justify-center">
+            <div className="mx-auto lg:w-1/2">
+              <div className="flex items-center justify-center">
+                <svg
+                  className="w-8 h-8 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-8 h-8 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-8 h-8 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-8 h-8 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  className="w-8 h-8 text-orange-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+              <blockquote className="mt-7">
+                <p className="text-xl leading-relaxed text-gray-800">
+                  “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam”
+                </p>
+              </blockquote>
+
+              <img
+                className="w-auto h-8 mx-auto mt-6 md:mt-10"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/testimonials/6/trivago.svg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>*/}
     </main>
   );
 }
